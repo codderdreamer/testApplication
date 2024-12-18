31d4e769d5afb3e99097a20ad82eb56e24474687
codderdreamer <sevdayucedagg@gmail.com>
2024-12-18 09:16:34 +0300

    t

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from src.configModule import *
 
 class Application():
     def __init__(self):
-        self.simu_test = True
+        self.simu_test = False
         self.modbus_ports = []                  # Aktif portlar
         self.aktive_eth = []                    # Aktif ethernetler
         self.modbus_connected = False           # Modbus bağlantısı
